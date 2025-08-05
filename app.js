77af06badd0c681a7491ab6ad211b9db7c769f22
Gabriel Cardoso <gabrlcardoso@hotmail.com>
2025-08-05 20:53:10 -0300

    Desafio Concluído

--- app.js
+++ app.js
@@ -1,1 +1,61 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+// Salvando os nomes
+
+function adicionarAmigo() {
+
+    let adicionar = document.getElementById("amigo").value;
+
+    if (adicionar == "") {
+
+        alert("Por favor, insira um nome.");
+
+    } else {
+
+        amigos.push(adicionar);
+
+        console.log(amigos);
+
+        document.getElementById("amigo").value = "";
+
+        // Colocando eles na lista visível
+
+        let lista = document.getElementById("listaAmigos");
+
+        lista.innerHTML = "";
+
+        for (let i = 0; i < amigos.length; i++) {
+
+            let nomes = document.createElement("li");
+
+            nomes.innerText = amigos[i];
+
+            lista.append(nomes);
+
+        }
+
+    }
+
+}
+
+// Sorteando o nome
+
+function sortearAmigo() {
+
+    if (amigos == "") {
+
+        alert("Você ainda não digitou o nome de ninguém.");
+
+    } else {
+
+        let n = Math.floor(Math.random() * amigos.length);
+
+        console.log(n);
+
+        let resultado = document.getElementById("resultado");
+
+        resultado.innerHTML = "O amigo secreto é: " + amigos[n];
+
+    }
+
+}
